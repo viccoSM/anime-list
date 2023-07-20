1b5ebfd0cc74d1daf835f50e85cdf672bc278153
Vicco Semuel Manopo <63226256+viccoSM@users.noreply.github.com>
2023-07-20 13:49:06 +0700

    feat(navbar): add header navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,23 +7,31 @@ const NavbarComponent = styled.div`
   padding: 16px 24px;
   display: flex;
   align-items: center;
-  justify-content: end;
-  gap: 14px;
+  justify-content: space-between;
   
-  & a {
-    font-weight: 600;
-    font-size: 16px;
-    color: ${colors.primary};
-    &:hover{
-      color: ${colors.secondary};
+  & .navbar-menu{
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    gap: 14px;
+    & a {
+      font-weight: 600;
+      font-size: 16px;
+      color: ${colors.primary};
+      &:hover{
+        color: ${colors.secondary};
+      }
     }
   }
 `
 
 const Navbar = () => (
   <NavbarComponent>
-    <Link href="/">List</Link>
-    <Link href="/collections">Collection</Link>
+    <h2>Anime</h2>
+    <div className="navbar-menu">
+      <Link href="/">List</Link>
+      <Link href="/collections">Collection</Link>
+    </div>
   </NavbarComponent>
 );
 
